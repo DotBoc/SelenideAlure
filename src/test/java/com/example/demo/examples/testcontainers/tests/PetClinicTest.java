906eb29aa80d27aa391249be405201f0b74863fa
test-user <test-user@test-user.com>
2024-09-17 18:45:09 +0300

    Added a fail test

--- src/test/java/com/example/demo/examples/testcontainers/tests/PetClinicTest.java
+++ src/test/java/com/example/demo/examples/testcontainers/tests/PetClinicTest.java
@@ -26,8 +26,8 @@ public class PetClinicTest extends UIBaseConfig {
     @Tag("UI")
     @Tag("SMK")
     @Severity(CRITICAL)
-    @Issue("ERMIS-46048")
-    @TmsLink("ERMIS-46032")
+    @Issue("JiraId-46048")
+    @TmsLink("JiraId-46032")
     public void userCanCreateOwners() {
         Allure.getLifecycle().updateTestCase(result -> result.setName("User can Create Owners"));
         new HomePage().goToFindOwners().addNewOwner();
@@ -44,13 +44,31 @@ public class PetClinicTest extends UIBaseConfig {
     @Tag("UI")
     @Tag("SMK")
     @Severity(CRITICAL)
-    @Issue("ERMIS-46048")
-    @TmsLink("ERMIS-46032")
+    @Issue("JiraId-46048")
+    @TmsLink("JiraId-46032")
     public void userCanReadOwners() {
         Allure.getLifecycle().updateTestCase(result -> result.setName("User can Read Owners"));
         new HomePage().goToFindOwners().searchForOwners();
         new Owners().assertOwner("Lucky",2);
         new Owners().assertOwner("Eduardo Rodriquez 	2693 Commerce St. 	McFarland 	6085558763 	Jewel Rosy",1);
+    }
+
+    @Test(groups = {"petClinic", "UI"})
+    @Epic("Pet Clinic")
+    @Feature("Owners")
+    @Story("Owners CRUD operations")
+    @Description("This test adds an owner using UI and asserts that addition")
+    @io.qameta.allure.Owner("gtrifyll")
+    @Tag("UI")
+    @Tag("SMK")
+    @Severity(CRITICAL)
+    @Issue("JiraId-46048")
+    @TmsLink("JiraId-46032")
+    public void failOnPurpose() {
+        Allure.getLifecycle().updateTestCase(result -> result.setName("This will fail"));
+        new HomePage().goToFindOwners().addNewOwner();
+        new NewOwner().addNewOwner("testFirst", "testLast", "testAdd", "testCity", "1234567890");
+        new Owner().assertOwnerInfo("testFirst testLast", "testAdd", "testCity", "test");
     }
 
 
